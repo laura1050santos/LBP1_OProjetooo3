e8d1b6c2dc5d6ca573dbd42827d9f2916a0c8cd9
laura1050santos <64499700+laura1050santos@users.noreply.github.com>
2024-10-18 07:49:09 -0300

    Delete controllers/controler.py

--- controllers/controler.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, Blueprint
-from models.model import p1,lst_users
-
-app_inicial = Blueprint('app', __name__) 
-
-@app_inicial.route('/')
-def hello_word():
-    
-    return render_template('index.html')
